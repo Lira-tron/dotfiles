ce0133f43b5ab9aa7cef721653c7fd409967aae8
Liratron <octalimon@gmail.com>
2023-09-09 09:24:51 -0700

    Update lspconfig.lua

--- home/.config/nvim/lua/liratron/plugins/lsp/lspconfig.lua
+++ home/.config/nvim/lua/liratron/plugins/lsp/lspconfig.lua
@@ -97,6 +97,24 @@ return {
             },
         })
 
+        local configs = require 'lspconfig.configs'
+
+        -- Check if the config is already defined (useful when reloading this file)
+        if not configs.barium then
+            configs.barium = {
+                default_config = {
+                    cmd = {'barium'};
+                    filetypes = {'brazil-config'};
+                    root_dir = function(fname)
+                        return lspconfig.util.find_git_ancestor(fname)
+                    end;
+                    settings = {};
+                };
+            }
+        end
+
+        lspconfig.barium.setup {}
+
         vim.diagnostic.config({
             virtual_text = false,
             severity_sort = true,
