98f3dd30514b480410c77237f84bf722e4e1baea
Liratron <octalimon@gmail.com>
2023-09-11 15:41:07 -0700

    adding markdown

--- home/.config/nvim/lua/liratron/plugins/markdown-preview.lua
+++ home/.config/nvim/lua/liratron/plugins/markdown-preview.lua
@@ -1,3 +1,8 @@
 return {
-  'iamcco/markdown-preview.nvim',
+    "iamcco/markdown-preview.nvim",
+    dependencies = { "zhaozg/vim-diagram", "aklt/plantuml-syntax" },
+    event = "BufRead",
+    build = function()
+        vim.fn["mkdp#util#install"]()
+    end,
 }
