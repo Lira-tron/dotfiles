c85d223bf80302e4b21422e1498b91b1c671e3af
Liratron <octalimon@gmail.com>
2024-01-15 12:11:31 -0800

    Adding vscode

--- home/.config/nvim/lua/liratron/plugins/formatting.lua
+++ home/.config/nvim/lua/liratron/plugins/formatting.lua
@@ -5,34 +5,34 @@ return {
   config = function()
     local conform = require("conform")
 
-        conform.setup({
-            formatters_by_ft = {
-                javascript = { "prettier" },
-                typescript = { "prettier" },
-                javascriptreact = { "prettier" },
-                typescriptreact = { "prettier" },
-                svelte = { "prettier" },
-                css = { "prettier" },
-                html = { "prettier" },
-                json = { "prettier" },
-                yaml = { "prettier" },
-                markdown = { "prettier" },
-                graphql = { "prettier" },
-                lua = { "stylua" },
-                python = { "isort", "black" },
-                go = {"goimports_reviser", "gofumpt", "golines"},
-                java ={"google-java-format"}
-            },
-            format_on_save = function(bufnr)
-                -- Disable with a global or buffer-local variable
-                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                    return
-                end
-                return { timeout_ms = 1000,async = false, lsp_fallback = false }
-            end
+    conform.setup({
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        svelte = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        graphql = { "prettier" },
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        go = { "goimports_reviser", "gofumpt", "golines" },
+        java = { "google-java-format" },
+      },
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 1000, async = false, lsp_fallback = false }
+      end,
     })
 
-    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+    vim.keymap.set({ "n", "v" }, "<leader>rF", function()
       conform.format({
         lsp_fallback = true,
         async = false,
