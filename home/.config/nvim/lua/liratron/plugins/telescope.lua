cf28a99ce9c7efa23a237f36b2543be7107a3e0c
Lira-tron <octalimon@gmail.com>
2024-01-20 07:41:35 -0800

    Update telescope.lua

--- home/.config/nvim/lua/liratron/plugins/telescope.lua
+++ home/.config/nvim/lua/liratron/plugins/telescope.lua
@@ -12,7 +12,14 @@ return {
     local actions = require("telescope.actions")
 
     telescope.setup({
-
+       extensions = {
+         fzf = {
+         fuzzy = true,                    -- false will only do exact matching
+         override_generic_sorter = true,  -- override the generic sorter
+         override_file_sorter = true,     -- override the file sorter
+         case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+      },
       layout_config = {
         width = 0.80,
         prompt_position = "top",
@@ -68,8 +75,9 @@ return {
       },
     })
 
-    telescope.load_extension("fzf")
     telescope.load_extension("file_browser")
+    telescope.load_extension("fzf")
+    
     -- set keymaps
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>s?", builtin.oldfiles, { desc = "[?] Find recently opened files" })
