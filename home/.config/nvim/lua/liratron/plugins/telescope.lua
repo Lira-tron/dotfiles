278dfc02c71c67a746c56738911d0cc03f880b67
Liratron <octalimon@gmail.com>
2024-01-21 10:30:51 -0800

    Removing ReplaceWithRegister

--- home/.config/nvim/lua/liratron/plugins/telescope.lua
+++ home/.config/nvim/lua/liratron/plugins/telescope.lua
@@ -12,13 +12,13 @@ return {
     local actions = require("telescope.actions")
 
     telescope.setup({
-       extensions = {
-         fzf = {
-         fuzzy = true,                    -- false will only do exact matching
-         override_generic_sorter = true,  -- override the generic sorter
-         override_file_sorter = true,     -- override the file sorter
-         case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
+      extensions = {
+        fzf = {
+          fuzzy = true, -- false will only do exact matching
+          override_generic_sorter = true, -- override the generic sorter
+          override_file_sorter = true, -- override the file sorter
+          case_mode = "smart_case", -- or "ignore_case" or "respect_case"                                 -- the default case_mode is "smart_case"
+        },
       },
       layout_config = {
         width = 0.80,
@@ -77,7 +77,7 @@ return {
 
     telescope.load_extension("file_browser")
     telescope.load_extension("fzf")
-    
+
     -- set keymaps
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>s?", builtin.oldfiles, { desc = "[?] Find recently opened files" })
