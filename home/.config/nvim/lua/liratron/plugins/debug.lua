2b4fda3d4f0de8744615cff6d95656d23b9afce9
Octavio 🍋 🥑 <limonoct@amazon.com>
2024-04-03 12:53:56 -0700

    Adding nvim nio

--- home/.config/nvim/lua/liratron/plugins/debug.lua
+++ home/.config/nvim/lua/liratron/plugins/debug.lua
@@ -38,7 +38,7 @@ return {
   },
   {
     "rcarriga/nvim-dap-ui",
-    dependencies = { "mfussenegger/nvim-dap" },
+    dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
     config = function()
       local dap = require("dap")
       local dapui = require("dapui")
