4af1af835b9bce9133c734d698adb049b57430ef
Liratron 🍋 🥑 <octalimon@gmail.com>
2024-05-05 09:18:03 -0700

    Adding gruvbox-flat

--- home/.config/nvim/lua/liratron/plugins/colorscheme.lua
+++ home/.config/nvim/lua/liratron/plugins/colorscheme.lua
@@ -2,6 +2,7 @@ return {
   {
     -- "jnurmine/Zenburn",
     -- "sainnhe/gruvbox-material",
+    -- "eddyekofo94/gruvbox-flat.nvim"
     "GwHisHere/concoctis.nvim",
 
     priority = 1000, -- make sure to load this before all the other start plugins
