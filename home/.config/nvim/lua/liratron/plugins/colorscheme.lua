1dbe3f226fbfba8675496672e62f4c107394cebd
Liratron 🍋 🥑 <octalimon@gmail.com>
2024-05-04 17:13:26 -0700

    Adding concoctis theme

--- home/.config/nvim/lua/liratron/plugins/colorscheme.lua
+++ home/.config/nvim/lua/liratron/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
 return {
   {
-    "sainnhe/gruvbox-material",
+    -- "sainnhe/gruvbox-material",
+    "GwHisHere/concoctis.nvim",
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
-      vim.cmd([[colorscheme gruvbox-material]])
+      vim.cmd([[colorscheme concoctis]])
     end,
   },
 }
