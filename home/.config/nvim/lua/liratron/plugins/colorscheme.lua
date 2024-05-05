7d2633aad46378cc0b6a28e8fa051324d71d20ca
Liratron 🍋 🥑 <octalimon@gmail.com>
2024-05-05 09:03:03 -0700

    Setting transparency

--- home/.config/nvim/lua/liratron/plugins/colorscheme.lua
+++ home/.config/nvim/lua/liratron/plugins/colorscheme.lua
@@ -6,6 +6,9 @@ return {
 
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
+      require("concoctis").setup({
+        transparent = true,
+      })
       -- load the colorscheme here
       vim.cmd([[colorscheme concoctis]])
     end,
