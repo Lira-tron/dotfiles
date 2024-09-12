e2d46082c754cfa315914ef8b124308a22520400
Octavio 🍋 🥑 <limonoct@amazon.com>
2024-09-12 14:02:08 -0700

    Using sneak instead of leap

--- home/.config/nvim/lua/liratron/plugins/leap.lua
+++ home/.config/nvim/lua/liratron/plugins/leap.lua
@@ -1,8 +1,11 @@
 return {
-    -- general-purpose motion plugin
-    'ggandor/leap.nvim',
-    config = function()
-        vim.keymap.set('n', 's', '<Plug>(leap-forward-to)', { desc = 'Leap forward-to' })
-        vim.keymap.set('n', 'S', '<Plug>(leap-backward-to)', { desc = 'Leap backward-to' })
-    end
+    'justinmk/vim-sneak',
 }
+-- return {
+--     -- general-purpose motion plugin
+--     'ggandor/leap.nvim',
+--     config = function()
+--         vim.keymap.set('n', 's', '<Plug>(leap-forward-to)', { desc = 'Leap forward-to' })
+--         vim.keymap.set('n', 'S', '<Plug>(leap-backward-to)', { desc = 'Leap backward-to' })
+--     end
+-- }
