132ef41695f24f156ab66ede365b79654a6b887b
Lira-tron <octalimon@gmail.com>
2024-11-29 09:40:23 -0800

    Add mini

---
+++ home/.config/nvim/lua/liratron/plugins/mininvim.lua
@@ -0,0 +1,28 @@
+return {
+  "echasnovski/mini.nvim",
+  version = false,
+  config = function()
+    require("mini.icons").setup()
+    require("mini.move").setup({
+
+      keys = {
+        { "H", mode = "v", desc = "Move visual block left" },
+        { "L", mode = "v", desc = "Move visual block right" },
+        { "J", mode = "v", desc = "Move visual block down" },
+        { "K", mode = "v", desc = "Move visual block up" },
+      },
+      opts = {
+        mappings = {
+          -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+          left = "H",
+          right = "L",
+          down = "J",
+          up = "K",
+        },
+      },
+    })
+    require("mini.ai").setup()
+    require("mini.indentscope").setup()
+    require("mini.splitjoin").setup()
+  end,
+}
