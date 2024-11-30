35f09e4fb81607abd577d2024e855dd90700a182
Octavio 🍋 🥑 <limonoct@amazon.com>
2024-11-29 19:22:25 -0800

    Change maximizer shortcuts

--- home/.config/nvim/lua/liratron/plugins/jdtls.lua
+++ home/.config/nvim/lua/liratron/plugins/jdtls.lua
@@ -183,9 +183,9 @@ return {
       -- Lesser used LSP functionality
       nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
       nmap("gt", "<cmd>Lspsaga goto_type_definition<cr>", "[G]oto [T]ype Definitions")
-      nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-      nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-      nmap("<leader>wl", function()
+      nmap("gwa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+      nmap("gwr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+      nmap("gwl", function()
         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
       end, "[W]orkspace [L]ist Folders")
 
