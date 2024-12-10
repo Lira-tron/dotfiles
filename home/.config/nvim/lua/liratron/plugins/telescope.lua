3a431285be912082743b3131ae7d9c9176259e17
Octavio 🍋 🥑 <limonoct@amazon.com>
2024-12-10 09:38:01 -0800

    Adds ivy to telescope

--- home/.config/nvim/lua/liratron/plugins/telescope.lua
+++ home/.config/nvim/lua/liratron/plugins/telescope.lua
@@ -1,9 +1,9 @@
 return {
   "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
+  tag = "0.1.8",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release" },
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
     "nvim-telescope/telescope-ui-select.nvim",
     "nvim-telescope/telescope-file-browser.nvim",
@@ -12,19 +12,18 @@ return {
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
-
+    local telescopeThemes = require("telescope.themes")
     telescope.setup({
       extensions = {
         fzf = {
-          fuzzy = true,                   -- false will only do exact matching
+          fuzzy = true, -- false will only do exact matching
           override_generic_sorter = true, -- override the generic sorter
-          override_file_sorter = true,    -- override the file sorter
-          case_mode = "smart_case",       -- or "ignore_case" or "respect_case"                                 -- the default case_mode is "smart_case"
+          override_file_sorter = true, -- override the file sorter
+          case_mode = "smart_case", -- or "ignore_case" or "respect_case"                                 -- the default case_mode is "smart_case"
         },
         ["ui-select"] = {
-          require("telescope.themes").get_dropdown({}),
+          telescopeThemes.get_ivy({}),
         },
-
       },
       layout_config = {
         width = 0.80,
@@ -34,7 +33,7 @@ return {
         vertical = { mirror = false },
       },
       layout_strategy = "horizontal",
-      defaults = {
+      defaults = telescopeThemes.get_ivy({
         vimgrep_arguments = {
           "rg",
           "--color=never",
@@ -68,7 +67,7 @@ return {
         -- },
         sorting_strategy = "ascending",
         file_ignore_patterns = { "node_modules", "^./.git/" },
-      },
+      }),
       pickers = {
         live_grep = {
           layout_config = {
@@ -89,17 +88,15 @@ return {
 
     -- set keymaps
     local builtin = require("telescope.builtin")
-    local telescopeThemes = require("telescope.themes")
 
     vim.keymap.set("n", "<leader>s?", builtin.oldfiles, { desc = "[?] Find recently opened files" })
     vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "[ ] Find existing buffers" })
-    vim.keymap.set("n", "<leader>sc", function()
-      -- You can pass additional configuration to telescope to change theme, layout, etc.
-      builtin.current_buffer_fuzzy_find(telescopeThemes.get_dropdown({
-        winblend = 10,
-        previewer = false,
-      }))
-    end, { desc = "[S]earch  in current buffer" })
+    vim.keymap.set(
+      "n",
+      "<leader>sc",
+      builtin.current_buffer_fuzzy_find,
+      { desc = "[S]earch  in current buffer" }
+    )
 
     vim.keymap.set("n", "<leader>sgf", builtin.git_files, { desc = "Search Git [F]iles" })
     vim.keymap.set("n", "<leader>sgb", builtin.git_branches, { desc = "Search [G]it [B]ranches" })
@@ -112,6 +109,11 @@ return {
     vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
     vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
     vim.keymap.set("n", "<leader>sp", builtin.spell_suggest, { desc = "[S][P]ell" })
+    vim.keymap.set("n", "<leader>s,", function()
+      builtin.find_files({
+        cwd = vim.fn.stdpath("config"),
+      })
+    end, { desc = "Nvim Config" })
 
     local function telescope_buffer_dir()
       return vim.fn.expand("%:p:h")
@@ -119,18 +121,16 @@ return {
 
     -- file_browser
     vim.keymap.set("n", "<leader>sv", function()
-      telescope.extensions.file_browser.file_browser(
-        {
-          path = telescope_buffer_dir():match("oil:///(.*)"),
-          cwd = telescope_buffer_dir(),
-          respect_gitignore = false,
-          hidden = true,
-          grouped = true,
-          previewer = true,
-          initial_mode = "normal",
-          layout_config = { height = 40 },
-        }
-      )
+      telescope.extensions.file_browser.file_browser({
+        path = telescope_buffer_dir():match("oil:///(.*)"),
+        cwd = telescope_buffer_dir(),
+        respect_gitignore = false,
+        hidden = true,
+        grouped = true,
+        previewer = true,
+        initial_mode = "normal",
+        layout_config = { height = 40 },
+      })
     end, { desc = "[P]roject [V]iew" })
 
     -- OIL
