a0090ce1c3feecfe928818f9b7004694ead66c76
Lira-tron <octalimon@gmail.com>
2024-12-19 16:30:25 -0800

    Adds everforest theme

--- home/.config/nvim/lua/liratron/plugins/colorscheme.lua
+++ home/.config/nvim/lua/liratron/plugins/colorscheme.lua
@@ -3,15 +3,31 @@ return {
     -- "jnurmine/Zenburn",
     -- "sainnhe/gruvbox-material",
     -- "eddyekofo94/gruvbox-flat.nvim"
-    "GwHisHere/concoctis.nvim",
-
+    "sainnhe/everforest",
+    dependencies = {
+      "GwHisHere/concoctis.nvim",
+    },
+    lazy = false,
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      require("concoctis").setup({
-        transparent = true,
-      })
-      -- load the colorscheme here
-      vim.cmd.colorscheme "concoctis"
+      local function setEverestForest()
+        -- Optionally configure and load the colorscheme
+        -- directly inside the plugin declaration.
+        vim.g.everforest_background = "hard"
+        vim.g.everforest_transparent_background = 2
+        vim.g.everforest_better_performance = 1
+        vim.g.everforest_enable_italic = true
+        vim.cmd.colorscheme("everforest")
+      end
+
+      local function setConcoctis()
+        require("concoctis").setup({
+          transparent = true,
+        })
+        -- load the colorscheme here
+        vim.cmd.colorscheme("concoctis")
+      end
+      setEverestForest()
     end,
   },
 }
