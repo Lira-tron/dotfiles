7a419b7373e9c419891fb3b1fc7c9fe82abac064
Octavio 🍋 🥑 <limonoct@amazon.com>
2024-12-23 18:37:06 -0800

    Adds Blink lua

---
+++ home/.config/nvim/lua/liratron/plugins/blink.lua
@@ -0,0 +1,60 @@
+return {
+  {
+    "saghen/blink.cmp",
+    version = "*",
+    event = "InsertEnter",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      {
+        "saghen/blink.compat",
+        opts = {},
+        version = "*",
+        lazy = true,
+      },
+    },
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      keymap = { preset = "default" },
+      appearance = {
+        use_nvim_cmp_as_default = false,
+        nerd_font_variant = "mono",
+      },
+      completion = {
+        accept = {
+          auto_brackets = {
+            enabled = true,
+          },
+        },
+        menu = {
+          draw = {
+            gap = 2,
+            treesitter = { "lsp" },
+            columns = {
+              { "kind_icon", "label", "label_description", gap = 1 },
+              { "source_name" },
+            },
+          },
+        },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 100,
+        },
+        ghost_text = {
+          enabled = true,
+        },
+      },
+
+      signature = { enabled = true },
+
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+    },
+    opts_extend = {
+      "sources.compat",
+      "sources.default",
+    },
+  },
+}
--- home/.config/nvim/lua/liratron/plugins/lsp/lspconfig.lua
+++ home/.config/nvim/lua/liratron/plugins/lsp/lspconfig.lua
@@ -2,7 +2,6 @@ return {
   "neovim/nvim-lspconfig",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
     { "antosha417/nvim-lsp-file-operations", config = true },
     -- Lsp inlay hints
     "lvimuser/lsp-inlayhints.nvim",
@@ -43,8 +42,6 @@ return {
     -- import lspconfig plugin
     local lspconfig = require("lspconfig")
 
-    -- import cmp-nvim-lsp plugin
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
     -- configure lsp inlay hints
     local ih = require("lsp-inlayhints")
@@ -245,21 +242,14 @@ return {
       },
     }
 
-    -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    -- for nvim-ufo fold
-    capabilities.textDocument.foldingRange = {
-      dynamicRegistration = false,
-      lineFoldingOnly = true,
-    }
-    capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+    local capabilities = require('blink.cmp').get_lsp_capabilities()
 
     -- Ensure the servers above are installed
     local mason_lspconfig = require("mason-lspconfig")
 
     mason_lspconfig.setup_handlers({
       function(server_name)
-        require("lspconfig")[server_name].setup({
+        lspconfig[server_name].setup({
           capabilities = capabilities,
           on_attach = on_attach,
           settings = servers[server_name],
