7268ac1aaeb97a9499ee503acd705818ac948fae
Octavio 🍋 🥑 <limonoct@amazon.com>
2025-01-31 11:56:08 -0800

    Removing nvim tree, adding snacks

---
+++ home/.config/nvim/lua/liratron/plugins/snacks.lua
@@ -0,0 +1,28 @@
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    explorer = {
+      -- your explorer configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    picker = {
+      sources = {
+        explorer = {
+          -- your explorer picker configuration comes here
+          -- or leave it empty to use the default settings
+        }
+      }
+    }
+  },
+  keys = {
+    {
+      "<leader>pv",
+      function()
+        Snacks.explorer()
+      end,
+      desc = "[P]roject [V]iew",
+    },
+  },
+}
