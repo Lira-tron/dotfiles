6844d03fb64debdf5bfa5e0e244d666be6e69bdd
Octavio 🍋 🥑 <limonoct@amazon.com>
2025-04-07 22:56:14 -0700

    Typo

--- home/.config/nvim/lua/config/lazy.lua
+++ home/.config/nvim/lua/config/lazy.lua
@@ -28,7 +28,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
     { import = "lazyvim.plugins.extras.editor.mini-diff" },
-    { import = "lazyvim.plugins.extras.coding.mini-surrount" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.dap.core" },
     -- { import = "lazyvim.plugins.extras.test.core"},
     { import = "lazyvim.plugins.extras.editor.snacks_picker" },
