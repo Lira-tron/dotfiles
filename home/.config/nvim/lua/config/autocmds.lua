13e5d4cf1e8b7c63f71c88ba5cc0d493af26a148
Liratron 🍋 🥑 <octalimon@gmail.com>
2025-04-26 16:10:15 -0700

    enabling oil again

--- home/.config/nvim/lua/config/autocmds.lua
+++ home/.config/nvim/lua/config/autocmds.lua
@@ -117,3 +117,14 @@ vim.api.nvim_create_autocmd("FileType", {
     end)
   end,
 })
+
+-- Open preview in oil by default
+vim.api.nvim_create_autocmd("User", {
+  pattern = "OilEnter",
+  callback = vim.schedule_wrap(function(args)
+    local oil = require("oil")
+    if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
+      oil.open_preview()
+    end
+  end),
+})
