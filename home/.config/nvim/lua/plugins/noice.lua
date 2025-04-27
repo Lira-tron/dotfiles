ff119e314794faa7a1f02d5aaa8b1912d452aa7f
Liratron 🍋 🥑 <octalimon@gmail.com>
2025-04-27 06:33:38 -0700

    removing comments

--- home/.config/nvim/lua/plugins/noice.lua
+++ home/.config/nvim/lua/plugins/noice.lua
@@ -4,9 +4,6 @@ return {
     event = "VeryLazy",
     opts = {
       presets = {
-        -- This is the search bar or popup that shows up when you press /
-        -- Setting this to false makes it a popup and true the search bar at the bottom
-        -- search middle
         bottom_search = true,
       },
       cmdline = {
@@ -14,8 +11,6 @@ return {
         view = "cmdline",
       },
       messages = {
-        -- NOTE: If you enable messages, then the cmdline is enabled automatically.
-        -- This is a current Neovim limitation.
         enabled = true, -- enables the Noice messages UI
         view = "mini", -- default view for messages
         view_error = "mini", -- view for errors
@@ -24,17 +19,11 @@ return {
         view_search = "mini", -- view for search count messages. Set to `false` to disable
       },
       notify = {
-        -- Noice can be used as `vim.notify` so you can route any notification like other messages
-        -- Notification messages have their level and other properties set.
-        -- event is always "notify" and kind can be any log level as a string
-        -- The default routes will forward notifications to nvim-notify
-        -- Benefit of using Noice for this is the routing and consistent history view
         enabled = true,
         view = "mini",
       },
       lsp = {
         message = {
-          -- Messages shown by lsp servers
           enabled = true,
           view = "mini",
         },
