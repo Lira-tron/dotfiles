28886f695bedd0313424e5cd99cf23ce99b36629
Liratron 🍋 🥑 <octalimon@gmail.com>
2025-09-21 12:16:10 -0700

    Removes comment

--- home/.config/nvim/lua/plugins/disabled.lua
+++ home/.config/nvim/lua/plugins/disabled.lua
@@ -1,9 +1,6 @@
 local is_neovide = vim.g.neovide or false
 
 return {
-  -- -- If your search ends with an 'a' you enter insert mode and start modifying shit in the file, it sucks
-  -- -- I'm giving flash a 2nd chance, I use my j and k too much
-  -- { "folke/flash.nvim", enabled = false },
 
   -- This is the plugin that shows tabs, I don't need it as I use BufExplorer and snipe
   -- I enable the plugin only if the is_neovide variable is set to true
